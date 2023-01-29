205459419f5b26eca9c3ea7efc67d60da702bada
330i <enactirace@gmail.com>
2023-01-28 20:21:20 -0600

    Psuedo-merge camera

---
+++ lib/screens/camera.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Camera.dart extends StatelessWidget {
+  const Camera.dart({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
