3d81def1949bf2c089eec420bfabc9d3a12da0a0
Joey Lent <supercolbat@gmail.com>
2023-01-28 18:23:11 -0600

    Patch null error

--- lib/main.dart
+++ lib/main.dart
@@ -84,8 +84,10 @@ class _AppState extends State<App> {
             OrientationBuilder(builder: (context, orientation) {
               return Padding(
                 padding: EdgeInsets.only(
-                  top: (MediaQuery.of(context).size.longestSide -
-                          _controller.value.previewSize!.longestSide) /
+                  top: (_controller.value.previewSize != null
+                          ? MediaQuery.of(context).size.longestSide -
+                              _controller.value.previewSize!.longestSide
+                          : 0) /
                       4,
                 ),
                 // padding: orientation == Orientation.portrait
